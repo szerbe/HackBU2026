37fe72ba8ef07f45e93cb48d4715b5d2c509eca9
szerbe <shalinzerbe@gmail.com>
2026-03-07 13:39:59 -0500

    something works
    i hate frontend

---
+++ recipe-app/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+const Home = () => {
+
+  return (
+    <div className="flex min-h-screen">
+      <div
+        className={`flex-1 transition-all duration-300 ease-in-out rounded-sm-r`}
+      >
+        <div className="p-8">
+          <header className="bg-[var(--primary-color)] p-4 rounded-sm">
+            <h1 className="text-3xl font-bold">
+              <p className="text-white">Hi!</p>
+            </h1>
+          </header>
+          <main className="p-4">
+            <p>Output recipe here</p>
+          </main>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
