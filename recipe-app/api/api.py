0f345c435787d2abf88a5e80d09f9987e63839cf
szerbe <shalinzerbe@gmail.com>
2026-03-08 00:55:25 -0500

    api runs

--- recipe-app/api/api.py
+++ recipe-app/api/api.py
@@ -1,8 +1,10 @@
-import time
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/time')
-def get_current_time():
-    return {'tim': time.time()}
+@app.route('/api/status', methods=['GET'])
+def status_check():
+    return jsonify({"status": "OK", "message": "Server is running"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
