3453e53257b141aacde08e775cc6c17954aae3e4
Kristin Lucero <klucero2@binghamton.edu>
2026-03-07 19:57:52 -0500

    Image

--- recipe-app/src/pages/Home.jsx
+++ recipe-app/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import SearchBar from "../components/Searchbar";
-
+import cake from "./cake.svg";
+//import recipe from "../svgs/recipe.svg";
 
 const Home = () => {
 
@@ -18,7 +19,10 @@ const Home = () => {
         <div className="p-8">
           <header className="bg-(--primary-color-dark) p-4 rounded-sm">
             <h1 className="text-3xl font-bold">
-              <p className="text-center text-(--t-color)">Welcome to the Recipe Converter!</p>
+              <div className="flex text-center justify-content-center align-items-center gap-4">
+                <img src={cake} alt="Cake" className="w-9 h-10 display-flex" />
+                <p className="text-center text-(--t-color) display-flex">Welcome to the Recipe Converter!</p>
+              </div>
               <SearchBar  
                 Text="Put your URL here" 
                 className="text-center bg-(--secondary-color) p-4 rounded-lg"
